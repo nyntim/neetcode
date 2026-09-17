f6f719e88a966e0d21a6f736512e5ef9ba19311b
Tim Nguyen <tin025@ucsd.edu>
2026-09-16 20:50:17 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack == []: self.minStack.append(val)
+        else: self.minStack.append(min(self.minStack[-1], val))
+
+    def pop(self) -> None:
+        self.minStack.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
