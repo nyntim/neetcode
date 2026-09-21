be59bf63f99db835816e6a44f55271845737f52e
Tim Nguyen <tin025@ucsd.edu>
2026-09-20 22:30:47 -0700

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = []
+        for i in range(len(position)):
+            pairs.append((position[i], speed[i]))
+        pairs.sort(reverse=True)
+
+        stack = []
+        for pair in pairs:
+            t = (target - pair[0]) / pair[1]
+            if not stack or t > stack[-1]:
+                stack.append(t)
+        
+        return len(stack)
+            
